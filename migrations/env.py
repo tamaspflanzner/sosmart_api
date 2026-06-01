1b827f24e6d6a58e6680c1cd5c9dff7c9fc118b7
Niloo <niloofardelavari24@gmail.com>
2026-06-02 01:02:35 +0200

    Add Alembic migration support for Cloud Run

--- migrations/env.py
+++ migrations/env.py
@@ -9,6 +9,11 @@ from alembic import context
 # access to the values within the .ini file in use.
 config = context.config
 
+import os
+
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
